2c6341efba0ce9c9219ce729a896ed32faff628d
Jason Axelson <axelson@users.noreply.github.com>
2022-01-13 09:26:12 +0000

    Fix ci by fixing and adding some tests (#1)

--- lib/jax_utils/string_helpers.ex
+++ lib/jax_utils/string_helpers.ex
@@ -1,3 +1,18 @@
 defmodule JaxUtils.StringHelpers do
+  @doc """
+  Check if a string is the empty string or nil
+
+      iex> is_blank("")
+      true
+
+      iex> is_blank(nil)
+      true
+
+      iex> is_blank(" ")
+      false
+
+      iex> is_blank("I am that I am")
+      false
+  """
   defguard is_blank(value) when value == nil or value == ""
 end
--- test/jax_utils_test.exs
+++ test/jax_utils_test.exs
@@ -1,8 +1,4 @@
 defmodule JaxUtilsTest do
   use ExUnit.Case
   doctest JaxUtils
-
-  test "greets the world" do
-    assert JaxUtils.hello() == :world
-  end
 end
